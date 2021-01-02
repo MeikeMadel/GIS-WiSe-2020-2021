708003b3dc491757fc4fbba7676d27a9ce5b3143
MeikeMadel <72403558+MeikeMadel@users.noreply.github.com>
2021-01-02 15:12:14 +0100

    Kommentare zu Beispiellose

--- Aufgabe_3.1/server.ts
+++ Aufgabe_3.1/server.ts
@@ -2,14 +2,14 @@ import * as Http from "http";
 
 export namespace P_3_1Server {
     console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
+    let port: number = Number(process.env.PORT); //die Portnummer in der Varbiable port speichern
+    if (!port) //wenn die Portnummer eine andere ist, den Port 8100 nehmen
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let server: Http.Server = Http.createServer(); // einen Server herstellen und in der Variable server speichern
+    server.addListener("request", handleRequest); //einen Listener zu dem Server hinzufügen, der die Anfrage handlet, sofern eine da ist
+    server.addListener("listening", handleListen); 
+    server.listen(port);//Server horcht auf port
 
     function handleListen(): void {
         console.log("Listening");
@@ -17,10 +17,10 @@ export namespace P_3_1Server {
 
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+        console.log("I hear voices!"); //sofern eine Serveranfrage erfolgt, wird dies in der Konsole ausgegeben
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Header der Antwort des Servers
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //Sicherheitsmechanisem werden ausgeschalten
+        _response.write(_request.url); //Es wird die URL mit der die Anfrage gestellt wurde in die Antwort geschrieben
+        _response.end(); //die Antwort wird beendet
     }
 }
